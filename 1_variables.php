77dc2ab705e61d8f1d6626ff1dafa806770001a5
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-01-25 14:24:14 +0100

    first commit

---
+++ 1_variables.php
@@ -0,0 +1,42 @@
+<?php 
+/*
+|--------------------
+| Variables
+|--------------------
+*/
+
+// Structure ($ name = value ;)
+$example = 10;
+echo $example; // echo ($example);
+
+// Correct Name (alphbets, numbers and _)
+$example_two123 = 10;
+
+// Correct first letter (no number)
+$name = 1;
+$Name = 1;
+$_name = 1;
+
+// not correct 
+// $3example = 10;
+
+// case ($NAME  vs $name)
+$example = 10;
+$examplE = 20;
+
+
+// type of value
+$x = 10;
+$name = "variable";
+
+// assign new value
+$x = 10;
+$x = "hakim";
+
+echo ($x);
+
+/*
+|--------------------
+| End Of Variables
+|--------------------
+*/
---
+++ 2_dataType.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+|--------------------
+| DataType
+|--------------------
+*/
+
+
+/*
+| Main DataTaypes:
+|
+| String
+| Integer
+| Double or Float
+| Boolean
+| Array
+| Object
+| Null
+| Ressource
+*/
+
+// String
+$name = "amir";
+$name = 'amir';
+
+// String Single Quote vs Double Quotes
+$x = 10;
+$example_one = 'x is $x';
+$example_two = "x is $x";
+
+// Integer
+$x = 100;
+
+// Double vs Float
+$x = 10.5;
+
+// Boolean
+$x = true;
+$x = false;
+
+
+// Boolean Case
+// true TRUE True TruE
+
+
+// Array
+$x = 10;
+$y = 20;
+$x = 30;
+
+$array = [10, 20, 30, "hakim", 10.5, true, ["hakim", 50]];
+
+// Object
+$date = new DateTime();
+/* object = new class; */
+
+// Null
+$x = null;
+
+// Null Case
+/* null Null NULL nulL */
+
+// Resource
+/*  
+| Resource is external resources that we use in PHP like database and file.
+| All of them are considered as resources.
+*/
