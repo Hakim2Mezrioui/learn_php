0fba944fc9e5483a342debfdcaec3f28b8a56187
Hakim2Mezrioui <mezrioui.hakim@gmail.com>
2024-01-26 22:46:39 +0100

    match

---
+++ 7_match.php
@@ -0,0 +1,28 @@
+<?php
+/*
+|--------------------
+| match
+|--------------------
+*/
+
+$number = 15;
+$result = match($number) {
+    5 => "it's five",   
+    6 => "it's six",
+    default => "unknown",
+};
+echo $result;
+
+$result = match(true) {
+    $number == 15 => "yes it's correct",   
+    6 => "it's six",
+    default => "unknown",
+};
+echo $result;
+
+
+/*
+|--------------------
+| End match
+|--------------------
+*/
