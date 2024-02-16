39cedeacb46ba1db0b85e4996b793402a1c4080e
mezrioui hakim <mezrioui.hakim@gmail.com>
2024-02-16 22:27:26 +0100

    class

---
+++ 28_class.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+|--------------------
+| Class
+|--------------------
+*/
+
+/* definition */
+// class NameOfClass {}
+
+/* properties */
+// class NameOfClass {}
+
+
+// ______________________________________________ //
+
+
+/* access modifiers */
+
+// private
+// protected
+// public
+
+/* constructor */
+
+/* properties */
+
+/* this keyword */
+
+/*
+|--------------------
+| End of Class
+|--------------------
+*/
